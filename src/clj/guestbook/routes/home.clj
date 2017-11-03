b323d11c4ebc5707e9ef856f88c112b75b3cd157
Eduardo Marques <edhana@gmail.com>
2017-11-03 16:44:27 -0200

    Adding css and error messages for the form validation

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -1,13 +1,16 @@
 (ns guestbook.routes.home
   (:require [guestbook.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [guestbook.db.core :as db]))
+            [guestbook.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]))
 
-(defn home-page []
+(defn home-page [{:keys [flash]}]
   (layout/render
     "home.html"
-    {:messages (db/get-messages)}))
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))))
 
 (defn about-page []
   (layout/render "about.html"))
@@ -15,13 +18,23 @@
 (defn whitesheep-page []
   (layout/render "whitesheep.html"))
 
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
+
 (defn save-message! [{:keys [params]}]
-  (db/save-message!
-    (assoc params :timestamp (java.util.Date.)))
-    (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do (db/save-message!
+      (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
   (GET "/about" [] (about-page))
-  (GET "/whitesheep" [] (whitesheep-page)
-  (POST "/message" request (save-message! request))))
+  (GET "/whitesheep" [] (whitesheep-page))
+  (POST "/message" request (save-message! request)))
